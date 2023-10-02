1aa3f25a730614594ff8ecb67cee3201fffc3865
Billy <ismail.bilkisu@gmail.com>
2023-10-02 09:26:22 +0100

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <nav className="bg-white">
+      <div className="flex items-center font-medium justify-around">
+        <div className="z-50 p-5 md:w-auto w-full flex justify-between ">
+          <a href="/">
+            Logo
+          </a>
+          <div className="text-3xl md:hidden">
+            <button onClick={() => setOpen(!open)}>
+              {open ? <FaTimes /> : <FaBars />}
+            </button>
+          </div>
+        </div>
+        <ul className="md:flex hidden uppercase items-center gap-8 font-[Poppins]">
+          <li>
+            <a to="/" className="py-7 px-3 inline-block">
+              Home
+            </a>
+          </li>
+          <li>
+            <a to="/" className="py-7 px-3 inline-block">
+              Contact
+            </a>
+          </li>
+          <li>
+            <a to="/" className="py-7 px-3 inline-block">
+              About
+            </a>
+          </li>
+          <li>
+            <a to="/" className="py-7 px-3 inline-block">
+              testimonial
+            </a>
+          </li>
+        </ul>
+        <div className="md:flex hidden md:gap-8">
+          <button >SignUp</button>
+          <button>SignIn</button>
+        </div>
+        {/* Mobile nav */}
+        <ul
+          className={`
+        md:hidden bg-white fixed w-full top-0 overflow-y-auto bottom-0 py-24 pl-4
+        duration-500 ${open ? "left-0" : "left-[-100%]"}
+        `}
+        >
+          <li>
+            <a to="/" className="py-7 px-3 inline-block">
+              Home
+            </a>
+          </li>
+          <li>
+            <a to="/" className="py-7 px-3 inline-block">
+              Contact
+            </a>
+          </li>
+          <li>
+            <a to="/" className="py-7 px-3 inline-block">
+              About
+            </a>
+          </li>
+          <li>
+            <a to="/" className="py-7 px-3 inline-block">
+              testimonial
+            </a>
+          </li>
+          <div className="py-5 flex flex-col gap-12">
+            <button>SignUp</button>
+            <button>SignIn</button>
+          </div>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
